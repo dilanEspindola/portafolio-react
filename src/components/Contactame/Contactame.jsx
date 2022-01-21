2a6b4ef7d6a4a1b46f0bd4ad4c888bdd7ecc21d0
Dilan <dilanespindola30@gmail.com>
2022-01-20 21:00:44 -0500

    añadiendo pagina contactame

--- src/components/Contactame/Contactame.jsx
+++ src/components/Contactame/Contactame.jsx
@@ -1,9 +1,50 @@
 import React from "react";
 
+import Footer from "../Footer";
+import "./contactame.css";
+
 function Contactame() {
   return (
-    <div>
-      <h1>Contactame</h1>
+    <div className="pagina-contactame">
+      <div className="contenedor-contactame">
+        <h1 className="titulo-contactame">Contáctame</h1>
+        <div className="flex">
+          <p className="info-contacto">
+            Puedes contactarme en mis redes sociales:
+          </p>
+          <div className="redes-sociales-contactame">
+            <span className="linkedin iconos">
+              <a
+                href="https://www.linkedin.com/in/dilan-espindola-09b6b51a9/"
+                target="_blank"
+              >
+                <i class="fab fa-linkedin-in"></i>
+              </a>
+              <p>LinkedIn</p>
+            </span>
+            <span className="instagram iconos">
+              <a href="https://www.instagram.com/noiisys/" target="_blank">
+                <i class="fab fa-instagram"></i>
+              </a>
+              <p>Instagram</p>
+            </span>
+            <span className="git-hub iconos">
+              <a href="https://github.com/dilanEspindola" target="_blank">
+                <i class="fab fa-github"></i>
+              </a>
+              <p>GitHub</p>
+            </span>
+            <span className="whatsapp iconos">
+              <div className="icono-numero">
+                <i class="fab fa-whatsapp"></i>
+                <p>+573144530377</p>
+              </div>
+              <p>WhatsApp</p>
+            </span>
+          </div>
+        </div>
+      </div>
+      <Footer />
     </div>
   );
 }
